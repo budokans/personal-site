1ca08a51d2646db8be0619cf57717cd5dab70528
budokans <68995729+budokans@users.noreply.github.com>
2021-05-04 18:32:57 +1200

    Add JSON data, test imgs, Portfolio component

---
+++ components/Portfolio.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@chakra-ui/layout"
+
+
+const Portfolio = () => {
+  return (
+    <Grid>
+      Test
+    </Grid>
+  )
+}
+
+export default Portfolio;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import { Flex  } from "@chakra-ui/layout";
 import Header from "../components/Header";
+import Portfolio from "../components/Portfolio";
 
 const IndexPage = () => {
 
   return (
     <Flex direction="column" spacing="15" justifyContent={["flex-start", "center"]} alignItems={["flex-start", "center"]} h="100vh" py={6} px={4}>
-      
+
       <Header />
+
+      <Portfolio />
 
     </Flex>
   )
