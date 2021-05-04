a07df3fd1f062e9927f705a99058a0316e8498c1
budokans <68995729+budokans@users.noreply.github.com>
2021-05-05 01:45:17 +1200

    Standardise button dimensions

--- components/Links.tsx
+++ components/Links.tsx
@@ -12,21 +12,21 @@ export default function Links() {
       {/* Large Screens Email Link */}
       <WrapItem d={isLargerThan930 ? "inline-flex" : "none"}>
         <Tooltip placement="bottom" borderRadius="lg" py="1.5" closeDelay={1000} label={hasCopied ? "Copied!" : "Click to Copy!"}>
-          <Button borderRadius="2xl" h="9" fontSize="sm" onClick={onCopy}>Email</Button>
+          <Button borderRadius="2xl" h="7" fontSize="sm" onClick={onCopy}>Email</Button>
         </Tooltip>
       </WrapItem>
 
-      {/* Small Screens Email Link */}
+      {/*  Small Screens Email Link */}
       <WrapItem d={isLargerThan930 ? "none" : "inline-flex"}>
-        <a href="mailto:contact@stevenwebster.co" className="chakra-button css-16bj6lo" target="_blank" rel="noopener noreferrer">Email</a>
+        <a href="mailto:contact@stevenwebster.co" className="css-1infzl" target="_blank" rel="noopener noreferrer">Email</a>
       </WrapItem>
 
       <WrapItem>
-        <a className="chakra-button css-16bj6lo" href="https://github.com/budokans" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a className="css-1infzl" href="https://github.com/budokans" target="_blank" rel="noopener noreferrer">GitHub</a>
       </WrapItem>
 
       <WrapItem>
-        <a className="chakra-button css-16bj6lo" href="https://www.linkedin.com/in/steven-webster-developer/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a className="css-1infzl" href="https://www.linkedin.com/in/steven-webster-developer/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       </WrapItem>
 
     </Wrap>
