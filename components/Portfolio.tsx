9db2d32a34657774c225025c8a90ef24754e10ec
budokans <68995729+budokans@users.noreply.github.com>
2021-05-05 18:09:46 +1200

    Create MotionBox framer-motion component

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -5,9 +5,11 @@ import PortfolioItem from "./PortfolioItem"
 const Portfolio = ({data}: ProjectDataProps) => {
   return (
     <SimpleGrid columns={[1, 1, 2]} spacing={3} w={['full', '90%', '80%', '900px']} mt={20} columnGap={10} rowGap={8}>
+
       {data.map((project: ProjectInterface, idx: number) => {
         return <PortfolioItem project={project} idx={idx} key={idx} />
       })}
+      
     </SimpleGrid>
   )
 }
