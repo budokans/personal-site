427deea78799d4787eb705a32243227a0f006d89
budokans <68995729+budokans@users.noreply.github.com>
2021-05-06 13:45:27 +1200

    Fix wrong email link on initial render bug
    The useMediaQuery only runs once the site has rendered, so making the rendering of either the clipboard email btn or the mailto email button conditional upon a state variable and updating that variable via useEffect watching the isLargerThan930 variable ensures the components re-render correctly after initial render.

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,23 +1,29 @@
 import { Button } from "@chakra-ui/button";
 import { Wrap, WrapItem  } from "@chakra-ui/layout";
 import { Tooltip, useClipboard, useMediaQuery } from "@chakra-ui/react"
+import { useState, useEffect } from "react";
 
 export default function Links() {
   const {hasCopied, onCopy} = useClipboard("contact@stevenwebster.co");
   const [isLargerThan930] = useMediaQuery("(min-width: 930px)")
+  const [isLargeDevice, setIsLargeDevice] = useState(false);
+
+  useEffect(() => {
+    isLargerThan930 ? setIsLargeDevice(true) : setIsLargeDevice(false)
+  }, [isLargerThan930])
 
   return (
     <Wrap spacing="3">
 
       {/* Large Screens Email Link */}
-      <WrapItem d={isLargerThan930 ? "inline-flex" : "none"}>
+      <WrapItem d={isLargeDevice ? "inline-flex" : "none"}>
         <Tooltip placement="bottom" borderRadius="lg" py="1.5" closeDelay={1000} label={hasCopied ? "Copied!" : "Click to Copy!"}>
           <Button borderRadius="2xl" h="7" fontSize="sm" onClick={onCopy}>Email</Button>
         </Tooltip>
       </WrapItem>
 
       {/*  Small Screens Email Link */}
-      <WrapItem d={isLargerThan930 ? "none" : "inline-flex"}>
+      <WrapItem d={isLargeDevice ? "none" : "inline-flex"}>
         <a href="mailto:contact@stevenwebster.co" className="css-1infzl" target="_blank" rel="noopener noreferrer">Email</a>
       </WrapItem>
 
