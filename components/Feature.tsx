66f73617d5dadfd2b58ebce9b999d18b40fe775e
budokans <68995729+budokans@users.noreply.github.com>
2021-05-06 20:41:11 +1200

    Add SimpleBar and functional Close Button

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,5 +1,8 @@
 import { Box } from "@chakra-ui/layout";
 import { MotionBox } from "./MotionBox";
+import SimpleBar from 'simplebar-react';
+import { CloseButton } from "@chakra-ui/close-button";
+import { useFeatureContext } from "../lib/featureContext";
 
 type FeatureProps = {
   project: number,
@@ -25,17 +28,30 @@ const containerVariants = {
       type: "spring",
       mass: 0.3,
       damping: 8,
+      duration: 5
     }
   }
 }
 
 const Feature = ({project}: FeatureProps) => {
+  const {closeFeature} = useFeatureContext();
+
   return (
     <>
       <Box position="absolute" top={0} right={0} bottom={0} left={0} bg="gray.100" />
+      
+      <MotionBox position="fixed" left={0} right={0} bottom={0} maxWidth="930px" marginY="0" marginX="auto" height="100vh"  variants={containerVariants} initial="hidden" animate="visible" exit="exit">
+        
+        <Box height="full" bg="white" borderWidth="1px" borderColor="gray.300" borderStyle="solid" borderRadius="xl">
+          
+          <SimpleBar style={{ maxHeight: '100%' }}>
+            <CloseButton size="md" fontSize="16px" borderRadius="full" color="gray.900" bg="gray.100" p={0} top="10px" right="10px" onClick={closeFeature}  />
 
-      <MotionBox position="fixed" left={0} right={0} bottom={0} maxWidth="930px" marginY="0" marginX="auto" zIndex={2} height="100vh" bg="white" variants={containerVariants} initial="hidden" animate="visible" exit="exit">
-        Something
+          </SimpleBar>
+
+        </Box>
+
+
       </MotionBox>
     </>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import {ChakraProvider} from "@chakra-ui/react";
 import { FeatureContextProvider } from "../lib/featureContext";
+import 'simplebar/dist/simplebar.min.css';
 
 const App = ({Component, pageProps}: AppProps): React.ReactNode => {
   return (
