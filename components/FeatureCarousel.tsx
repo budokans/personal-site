91f767903657fab076740d392a58d3413fd18c1f
budokans <68995729+budokans@users.noreply.github.com>
2021-05-09 01:58:04 +1200

    Create draggable carousel for large screens
    and conditionally render at of above width = 930px

--- components/FeatureCarousel.tsx
+++ components/FeatureCarousel.tsx
@@ -1,5 +1,7 @@
 import { Img } from "@chakra-ui/image";
 import { Box, Stack } from "@chakra-ui/layout"
+import { useMediaQuery } from "@chakra-ui/media-query";
+import { motion } from "framer-motion";
 import { MotionBox } from "./MotionBox";
 
 type FeatureCarouselProps = {
@@ -11,6 +13,12 @@ const hideScrollbar = {
     "width": "0 !important",
     "display": "none", 
   },
+}
+
+const disableUserSelect = {
+  "-webkit-user-select": "none",
+	"-webkit-user-drag": "none",
+	"-webkit-app-region": "noDrag",
 }
 
 const containerVariants = {
@@ -27,43 +35,82 @@ const containerVariants = {
 }
 
 const FeatureCarousel = ({media}: FeatureCarouselProps) => {
+  const [isLargerThan930] = useMediaQuery("(min-width: 930px)");
+  const MotionStack = motion(Stack)
+
   return (
     <Box overflow="hidden" px={[4, 9]}>
       <MotionBox variants={containerVariants} initial="hidden" animate="visible" >
 
-        <Stack 
-          direction="row"
-          alignItems="center"
-          spacing={4} 
-          height="calc(45vw + 35px)" 
-          maxH="490px" 
-          overflowX="scroll" 
-          whiteSpace="nowrap" 
-          sx={{ ...hideScrollbar, scrollSnapType: "x mandatory" }} 
-          cursor="grab"
-        >
-
-        { media.map((image, idx) => {
-          return (
-            <Box 
-              key={idx} 
-              bg="gray.400" 
-              borderRadius="xl"
-              height="100%"
-              minWidth="calc(70vw + 35px)"
-              sx={{ scrollSnapAlign: "start", scrollPadding: "1.75rem" }} 
-              p="7.5vw"
+        { isLargerThan930 ? (
+          <Box>
+            <MotionStack
+              drag="x"
+              dragConstraints={{left: -3800, right: 0}}
+              dragElastic={0.5}
+              direction="row"
+              alignItems="center"
+              spacing={4} 
+              height="490px"
+              whiteSpace="nowrap" 
+              sx={{ ...hideScrollbar, scrollSnapType: "x mandatory" }} 
+              cursor="grab"
             >
+              { media.map((image, idx) => {
+                return (
+                  <Box 
+                    key={idx} 
+                    border="1px solid black"
+                    bg="gray.400" 
+                    borderRadius="2xl"
+                    height="100%"
+                    minWidth="763px"
+                    sx={{ scrollSnapAlign: "start", scrollPadding: "1.75rem" }} 
+                    p="7.5vw"
+                  >
+                    <Box h="100%" w="100%" >        
+                      <Img src={image} objectFit="cover" h="100%" w="100%" sx={disableUserSelect} />
+                    </Box>
+                  </Box>
+                )
+              })}
+            </MotionStack>
+          </Box>
+      
+        ) : ( 
+          <Stack 
+            direction="row"
+            alignItems="center"
+            spacing={4} 
+            height="calc(45vw + 35px)" 
+            maxH="490px" 
+            overflowX="scroll" 
+            whiteSpace="nowrap" 
+            sx={{ ...hideScrollbar, scrollSnapType: "x mandatory" }} 
+            cursor="grab"
+          >
 
-              <Box h="100%" w="100%" >        
-                <Img src={image} objectFit="cover" h="100%" w="100%" />
-              </Box>
-              
-            </Box>
-          )
-        })}
+            { media.map((image, idx) => {
+              return (
+                <Box 
+                  key={idx} 
+                  bg="gray.400" 
+                  borderRadius="xl"
+                  height="100%"
+                  minWidth="calc(70vw + 35px)"
+                  sx={{ scrollSnapAlign: "start", scrollPadding: "1.75rem" }} 
+                  p="7.5vw"
+                >
 
-        </Stack>
+                  <Box h="100%" w="100%" >        
+                    <Img src={image} objectFit="cover" h="100%" w="100%" />
+                  </Box>
+
+                </Box>
+              )
+            })}
+          </Stack>
+        )}
       </MotionBox>
     </Box>
   )
