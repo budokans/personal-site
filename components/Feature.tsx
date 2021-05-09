d748e54cc4163983999bca54f6c668201f027f31
budokans <68995729+budokans@users.noreply.github.com>
2021-05-09 16:56:26 +1200

    Animate height on small devices
    Testing to see if this will prevent the browser address bar on mobile devices from being included in the final sizing.

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -10,6 +10,7 @@ import FeatureTech from "./FeatureTech";
 import FeatureDescription from "./FeatureDescription";
 import FeatureVisit from "./FeatureVisit";
 import FeatureCarousel from "./FeatureCarousel";
+import { useMediaQuery } from "@chakra-ui/media-query";
 
 type FeatureProps = {
   project: ProjectInterface
@@ -27,7 +28,7 @@ const overlayVariants = {
   }
 }
 
-const containerVariants = {
+const containerVariantsLg = {
   hidden: {
     y: "100vh"
   },
@@ -51,22 +52,94 @@ const containerVariants = {
   }
 }
 
+const containerVariantsSm = {
+  hidden: {
+    height: "0%"
+  },
+  visible: {
+    height: "95%",
+    transition: {
+      type: "spring",
+      mass: 0.3,
+      damping: 10,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+  exit: {
+    height: "0%",
+    transition: {
+      type: "spring",
+      mass: 0.3,
+      damping: 8,
+    }
+  }
+}
+
 const Feature = ({project}: FeatureProps) => {
   const {showFeature, closeFeature, node} = useFeatureContext();
+  const [isLargerThan930] = useMediaQuery("(min-width: 930px)");
 
   return (
     
       <AnimatePresence >
         { showFeature && (
           <>
-            <MotionBox position="absolute" top={0} right={0} bottom={0} left={0} bg="gray.400" variants={overlayVariants} initial="hidden" animate="visible" exit="hidden"  />
+            {/* Overlay */}
+            <MotionBox 
+              position="absolute" 
+              top={0} 
+              right={0} 
+              bottom={0} 
+              left={0} 
+              bg="gray.400" 
+              variants={overlayVariants} 
+              initial="hidden" 
+              animate="visible" 
+              exit="hidden"  
+            />
+
+            {/* Container */}
+            <MotionBox 
+              position="fixed" 
+              left={0} 
+              right={0} 
+              bottom={0} 
+              maxWidth="930px" 
+              marginY="0" 
+              marginX="auto" 
+              height="100vh" 
+              variants={isLargerThan930 ? containerVariantsLg : containerVariantsSm} 
+              initial="hidden" 
+              animate="visible" 
+              exit="exit"
+            >
         
-            <MotionBox position="fixed" left={0} right={0} bottom={0} maxWidth="930px" marginY="0" marginX="auto" height="100vh"  variants={containerVariants} initial="hidden" animate="visible" exit="exit">
-              
-              <Box height="full" bg="white" borderWidth="1px" borderColor="gray.300" borderStyle="solid" borderRadius="xl" ref={node} >
+              <Box 
+                height="full" 
+                bg="white" 
+                borderWidth="1px" 
+                borderColor="gray.300" 
+                borderStyle="solid" 
+                borderRadius="xl" 
+                ref={node} 
+              >
                 
                 <SimpleBar style={{ maxHeight: '100%' }}>
-                  <CloseButton w={["28px", "34px"]} h={["28px", "34px"]} fontSize="14px" borderRadius="full" color="gray.900" bg="gray.100" p={0} position="absolute" top={["10px", "30px"]} right={["10px", "30px"]} onClick={closeFeature}  />
+                  
+                  <CloseButton 
+                    w={["28px", "34px"]} 
+                    h={["28px", "34px"]} 
+                    fontSize="14px" 
+                    borderRadius="full" 
+                    color="gray.900" 
+                    bg="gray.100" 
+                    p={0} 
+                    position="absolute" 
+                    top={["10px", "30px"]} 
+                    right={["10px", "30px"]} 
+                    onClick={closeFeature}  
+                  />
                   
                     <FeatureHeader project={project} />
 
