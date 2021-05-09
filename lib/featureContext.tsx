ee566c435ad3778fc6c21056e1018e457ac727b1
budokans <68995729+budokans@users.noreply.github.com>
2021-05-09 17:05:10 +1200

    Separate concerns: createCtx()

--- lib/featureContext.tsx
+++ lib/featureContext.tsx
@@ -1,18 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { ContextType } from "../interfaces"
-
-export function createCtx<ContextType>() {
-  const ctx = React.createContext<ContextType | undefined>(undefined);
-  function useCtx() {
-    const c = React.useContext(ctx);
-    if (!c)
-      throw new Error(
-        "useCtx must be inside a Provider with a value"
-      );
-    return c;
-  }
-  return [useCtx, ctx.Provider] as const;
-}
+import { createCtx } from './createCtx';
 
 const [useFeatureContext, CtxProvider] = createCtx<ContextType>();
 export { useFeatureContext };
