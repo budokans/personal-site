d9a12457087952fc9520b755851f2f593da9b96c
Steven Webster <swebsterdev@gmail.com>
2023-10-11 00:01:45 +1300

    Remove redundant .babelrc, configure nextJest Next.js 12+ uses SWC so no need for .babelrc if Jest is configured correctly.

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require("next/jest");
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: "./" });
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  collectCoverageFrom: [
+    "<rootDir>/components/feature/**/*.tsx",
+    "!<rootDir>/components/feature/descriptions/*.tsx",
+    "<rootDir>/components/home/**/*.tsx",
+    "<rootDir>/containers/**/*.tsx",
+    "<rootDir>/hooks/**/*.tsx",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  coverageReporters: ["html", "text"],
+  roots: ["<rootDir>"],
+  modulePaths: ["<rootDir>"],
+  moduleDirectories: ["node_modules", "utils"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/__tests__/testData"],
+};
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig);
