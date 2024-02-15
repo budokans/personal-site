9ce0e20e45c15bb02927189f22e02f885f0f9f7e
Steven Webster <swebsterdev@gmail.com>
2024-02-15 23:32:16 +1300

    add imaginetees.ai portfolio feature

--- components/feature/descriptions/index.tsx
+++ components/feature/descriptions/index.tsx
@@ -1,3 +1,4 @@
 export { PivotPointeDescription as PivotPointe } from "./PivotPointe";
 export { PortfolioSiteDescription as PortfolioSite } from "./PortfolioSite";
 export { StoryTyperDescription as StoryTyper } from "./StoryTyper";
+export { ImagineTeesDescription as ImagineTees } from "./ImagineTees";
